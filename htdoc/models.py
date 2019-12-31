fdaf928c1d83e4cb6788c6eb9597b11d3acb44e1
bmulas1535 <bmulas1535@users.noreply.github.com>
2019-12-31 13:08:14 -0800

    fix user model relationship

--- htdoc/models.py
+++ htdoc/models.py
@@ -10,7 +10,7 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('BlogPost', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}')."

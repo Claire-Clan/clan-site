739df9bad6b55d4ddb2ee754687954e743c73352
bmulas1535 <bmulas1535@users.noreply.github.com>
2019-12-31 17:14:08 -0800

    fix forms import

--- htdoc/forms.py
+++ htdoc/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-from fast.models import User
+from wtforms.validators import DataRequired, Length
+from htdoc.models import User
 
 class LoginForm(FlaskForm):
     username = StringField(
